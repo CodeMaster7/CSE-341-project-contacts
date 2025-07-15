059d7cfeafc9bfd2512948cd1f6e01d241f9e43c
Sam <sammybonfanti@gmail.com>
2025-07-14 21:47:55 -0700

    Add swagger-gen.js to generate API documentation using swagger-autogen

---
+++ backend/swagger-gen.js
@@ -0,0 +1,24 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+// Basic info about your API
+const doc = {
+	info: {
+		title: 'Contacts API',
+		description: 'Simple API for managing contacts',
+		version: '1.0.0'
+	},
+	host: 'localhost:3000',
+	schemes: ['http'],
+	basePath: '/'
+}
+
+// Where to create the swagger.json file
+const outputFile = './swagger.json'
+
+// Which files contain your routes
+const endpointsFiles = [
+	'./routes/index.js' // This will scan your routes through index.js
+]
+
+// Generate the swagger documentation
+swaggerAutogen(outputFile, endpointsFiles, doc)
