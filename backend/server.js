b6c0c2bd158ead1338f71db4cd89a40122fece46
Sam <sammybonfanti@gmail.com>
2025-07-14 22:52:50 -0700

    Add CORS middleware to server.js for cross-origin resource sharing

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // Simple Express + MongoDB server
 const express = require('express')
+const cors = require('cors')
 const { initDatabase } = require('./data/database')
 const routes = require('./routes')
 require('dotenv').config()
@@ -8,6 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Middleware
+app.use(cors())
 app.use(express.json())
 
 // Routes
